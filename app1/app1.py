c28e4621a34432a129b0eb4af36aed7b98333097
LevKesha <zxcv02@gmail.com>
2022-07-12 11:29:43 +0000

    almost

--- app1/app1.py
+++ app1/app1.py
@@ -5,11 +5,11 @@ import socket
 hostname = socket.gethostname()    
 IPAddr = socket.gethostbyname(hostname)
 
-app = Flask('app')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', hostname=hostname, IPAddr=IPAddr)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
