61c1b8ed5e6507897311143591417973b96bd4d5
LevKesha <root@ip-172-31-5-128.us-east-2.compute.internal>
2022-07-12 08:39:13 +0000

    with flask

--- app1/app1.py
+++ app1/app1.py
@@ -1,5 +1,15 @@
-import socket    
+from flask import Flask, render_template
+import socket
+
+#Get IP & Hostname
 hostname = socket.gethostname()    
-IPAddr = socket.gethostbyname(hostname)    
-print("Your Computer Name is:" + hostname)    
-print("Your Computer IP Address is:" + IPAddr) 
+IPAddr = socket.gethostbyname(hostname)
+
+app = Flask('app')
+
+@app.route('/')
+def index():
+    return render_template('index.html', hostname=hostname, IPAddr=IPAddr)
+
+if __name__ == '__main__':
+    app.run()
