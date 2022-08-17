e556c1384834f01a9b4495470b77def479c46ae3
levkesha <zxcv02@gmail.com>
2022-08-17 12:50:32 +0300

    finak

--- app1/app1.py
+++ app1/app1.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
+from datetime import date, datetime
+from meteostat import Point, Daily
 import socket
 
 #Get IP & Hostname
 hostname = socket.gethostname()    
 IPAddr = socket.gethostbyname(hostname)
 
+start = datetime(date.today())
+end = datetime(date.today())
+
+telaviv = Point(32.0853, 34.7818, 5)
+
+cel = Daily(telaviv, start, end)
+cel = cel.fetch()
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', hostname=hostname, IPAddr=IPAddr)
+    return render_template('index.html', hostname=hostname, IPAddr=IPAddr, cel=cel)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
